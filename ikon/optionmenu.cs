8e036501f9bb904665cbfafb93c87d48c80fc4f7
Fey24 <fentyka24@gmail.com>
2019-04-24 07:20:12 +0700

    Add files via upload

---
+++ ikon/optionmenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections; 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class optionmenu : MonoBehaviour {
+
+    public void optiontomenu (int Option){
+        SceneManager.LoadScene(Option);
+    }
+}
