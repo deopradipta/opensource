7dc12080437eb076bf8b0abbcfe0c22c9f829205
Deo Pradipta <deopradipta2010@gmail.com>
2019-04-28 21:28:29 +0700

    New

---
+++ ikon/btnFx.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnFx : MonoBehaviour {
+
+    public AudioSource myFx;
+    public AudioClip clickFx;
+
+    public void loadNextScene()
+    {
+        StartCoroutine(LoadngLevel());
+    } 
+    
+    IEnumerator LoadngLevel()
+    {
+        yield return new WaitForSeconds(1f);
+        Application.LoadLevel("option");
+    }
+
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+}
